5a73f720f09a3506090a877055d9dbe29c1bfa57
Berkmann18 <maxieberkmann@gmail.com>
2019-04-30 18:19:44 +0100

    chore(extract): switched to `javascript-lemmatizer`
    And revised both extractors

--- src/extract.js
+++ src/extract.js
@@ -1,29 +1,26 @@
-// const extractLemma = require('extract-lemmatized-nonstop-words')
-// @todo find a lemmatizer as good as the defunct ^
+const Lem = require('javascript-lemmatizer')
+
+const lemmatizer = new Lem()
+
+const tokenize = input => input.split(/[ \t,;:.-_]/).filter(Boolean) //perhaps remove _ to keep emoji words joint
 
 // Feature extractor - a function that takes a sample and adds features to a given features set:
 const wordExtractor = (input, features) => {
   //similar to limdu.features.NGramsOfWords(1)
-  input
-    /* .replace(/[:\-_]/g, ' ') //normalisation
-    .replace(/\s{2,}/, ' ')
-    .split(' ') //tokenification */
-    .split(/[ \t,;:.-_]/) //perhaps remove _ to keep emoji words joint
-    .filter(Boolean)
-    //@todo replace the above lines by
-    .forEach(word => {
-      features[word.toLowerCase()] = 1
-    })
+  const tokens = tokenize(input)
+  tokens.forEach(word => {
+    features[word.toLowerCase()] = 1
+  })
+  return tokens
 }
 
-// const extract = (input, features) => {
-//   const extraction = extractLemma(input).map(ex =>
-//     'lemma' in ex ? ex.lemma : ex.normal,
-//   )
-//   extraction.forEach(word => {
-//     features[word] = 1
-//   })
-//   return extraction
-// }
+const extract = (input, features) => {
+  const tokens = tokenize(input)
+  const extraction = tokens.map(token => lemmatizer.only_lemmas(token)[0])
+  extraction.forEach(word => {
+    features[word] = 1
+  })
+  return extraction
+}
 
-module.exports = {wordExtractor}
+module.exports = {extract, wordExtractor}
