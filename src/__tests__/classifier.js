fe94fa6c8c607f3a016cd6968bdaf4a52023267a
Berkmann18 <maxieberkmann@gmail.com>
2019-04-30 12:02:01 +0100

    test(classifier): added tests

---
+++ src/__tests__/classifier.js
@@ -0,0 +1,12 @@
+const builder = require('../classifier')
+
+test('attributes', () => {
+  const classifier = builder()
+  expect(typeof classifier).toStrictEqual('object')
+  expect(typeof classifier.classifier).toStrictEqual('object')
+  expect(classifier.classifier.binaryClassifierType.name).toStrictEqual(
+    'bound WinnowHash',
+  )
+  expect(classifier.featureExtractors.name).toStrictEqual('wordExtractor')
+  expect(classifier.pastTrainingSamples).toEqual([])
+})
