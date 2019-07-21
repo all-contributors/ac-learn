577d984eff4cbf270b3c7adc555d407fd6ff1514
Berkmann18 <maxieberkmann@gmail.com>
2019-07-21 13:21:04 +0100

    chore(labels): sorted labels with a new module

--- src/__tests__/conv.js
+++ src/__tests__/conv.js
@@ -1,21 +1,23 @@
 const conv = require('../conv')
 
+const first = '.net core'
+
 test('io/limdu', () => {
   const data = conv('io')
   expect(data.length > 100).toBeTruthy()
-  expect(data[0].input).toStrictEqual('0 - backlog')
-  expect(data[0].output).toStrictEqual('null')
+  expect(data[0].input).toStrictEqual(first)
+  expect(data[0].output).toStrictEqual('code')
 })
 
 test('arr', () => {
   const data = conv('arr')
   expect(data.length > 100).toBeTruthy()
-  expect(data[0]).toEqual(['0 - backlog', 'null'])
+  expect(data[0]).toEqual([first, 'code'])
 })
 
 test('normal', () => {
   const data = conv('')
   expect(data.length > 100).toBeTruthy()
-  expect(data[0].label).toStrictEqual('0 - backlog')
-  expect(data[0].category).toStrictEqual('null')
+  expect(data[0].label).toStrictEqual(first)
+  expect(data[0].category).toStrictEqual('code')
 })
--- src/__tests__/labels.js
+++ src/__tests__/labels.js
@@ -7,19 +7,19 @@ test('All data', () => {
   const data = labels.getAll()
   expect(data.length >= LEN).toBeTruthy()
   expect(Array.isArray(data)).toBeTruthy()
-  expect(data[9]).toEqual({label: ':bug: bug', category: 'bug'})
+  expect(data[9]).toEqual({label: '7.x: regression', category: 'bug'})
 })
 
 test('get', () => {
-  expect(labels.getAt(0)).toEqual({label: '0 - backlog', category: 'null'})
-  expect(labels.getAt(9)).toEqual({label: ':bug: bug', category: 'bug'})
+  expect(labels.getAt(0)).toEqual({label: '.net core', category: 'code'})
+  expect(labels.getAt(9)).toEqual({label: '7.x: regression', category: 'bug'})
 })
 
 test('Labels', () => {
   const lbls = labels.getLabels()
   expect(lbls.length >= LEN).toBeTruthy()
   expect(Array.isArray(lbls)).toBeTruthy()
-  expect(lbls[9]).toEqual(':bug: bug')
+  expect(lbls[9]).toEqual('7.x: regression')
 })
 
 test('Categories', () => {
@@ -41,19 +41,19 @@ test('Size', () => {
 test('Labels with categories', () => {
   const cl = labels.getCategorisedLabels()
   expect(cl.length > categories.length).toBeTruthy()
-  expect(cl[9]).toEqual({label: ':bug: bug', category: 'bug'})
+  expect(cl[9]).toEqual({label: '7.x: regression', category: 'bug'})
 })
 
 test('Labels with a `null` category', () => {
   const nl = labels.getNullCatLabels()
   expect(nl.length < LEN - categories.length).toBeTruthy()
-  expect(nl[0]).toEqual({label: '0 - backlog', category: 'null'})
+  expect(nl[3]).toEqual({label: '2.x', category: 'null'})
 })
 
 test('Labels with a valid category', () => {
   const vl = labels.getValidCatLabels()
   expect(vl.length > categories.length).toBeTruthy()
-  expect(vl[0]).toEqual({label: '0 - backlog', category: 'null'})
+  expect(vl[0]).toEqual({label: '.net core', category: 'code'})
 })
 
 test('Bad data', () => {
