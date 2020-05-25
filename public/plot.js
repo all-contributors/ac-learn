3644ecb67cbee831e4574ea9a33b5f5456f975a8
Berkmann18 <maxieberkmann@gmail.com>
2020-05-25 19:01:43 +0100

    feat: better plots
    Label distribution and t/v/t partitions to answer
    https://github.com/all-contributors/ac-learn/issues/20#issuecomment-633291162

--- public/plot.js
+++ public/plot.js
@@ -1,37 +1,130 @@
 /* eslint-disable no-console */
-const loadData = async () => {
-  try {
-    const data = await Promise.all([
-      // fetch('../src/categories.json').then(res => res.json(), console.error),
-      // fetch('../src/labels.json').then(res => res.json(), console.error),
-      fetch('../playground/categoryPartitions.json').then(
-        res => res.json(),
-        console.error,
-      ),
-    ])
+/* global Plotly */
+import {loadData, orderEntriesByValues, perc} from './utils.js'
 
-    return data //[categories, dataset, categoryPartitions]
-  } catch (error) {
-    console.log('Error downloading one or more files:', error)
+const labelDistributionPlot = data => {
+  const orderedData = orderEntriesByValues(
+    data,
+    ['overall', 'train', 'test', 'validation'],
+    ['asc', 'asc', 'asc', 'asc'],
+  )
+  // console.log('ordered:', orderedData)
+  const xVals = orderedData.map(entry => entry[1].overall)
+  const categoryDistPlot = {
+    y: orderedData.map(entry => entry[0]),
+    x: xVals,
+    type: 'bar',
+    orientation: 'h',
+    text: xVals.map(String),
+    textposition: 'outside',
+    hoverinfo: 'none',
+    // marker: {
+    //   color: 'rgb(158,202,225)',
+    //   opacity: 0.6,
+    //   line: {
+    //     color: 'rgb(8,48,107)',
+    //     width: 1.5
+    //   }
+    // }
   }
+
+  const vizData = [categoryDistPlot]
+  const layout = {
+    title: 'Label Distribution',
+    autosize: true,
+    font: {
+      size: 18,
+    },
+    height: 1000,
+    yaxis: {
+      title: 'Contribution categories (labels)',
+      automargin: true,
+    },
+    xaxis: {
+      title: 'Overall count',
+      type: 'log',
+      automargin: true,
+      tick0: 0,
+      dtick: Math.log10(1), //log10(e**1), 0.30102999566
+    },
+  }
+
+  Plotly.newPlot('plot0', vizData, layout, {scrollZoom: true})
+}
+
+const tvtPartitionsPlot = data => {
+  const tvtSplits = data
+    .map(entry => entry[1])
+    .reduce((acc, part) => {
+      for (const key in part) {
+        if (key in acc) acc[key] += part[key]
+        else acc[key] = part[key]
+      }
+      return acc
+    }, {})
+  console.log('tvt=', tvtSplits)
+
+  const xVals = Object.keys(tvtSplits).slice(1)
+  const yVals = Object.values(tvtSplits).slice(1)
+  const partitionsPlot = {
+    x: xVals,
+    y: yVals,
+    type: 'bar',
+    text: yVals.map(
+      y =>
+        `${y} (<span style="color: #00f">${perc(
+          y / tvtSplits.overall,
+        )}</span>)`,
+    ),
+    hoverinfo: 'none',
+    textposition: 'auto',
+    marker: {
+      color: 'rgb(158,202,225)',
+      opacity: 0.6,
+      line: {
+        color: 'rgb(8,48,107)',
+        width: 1.5,
+      },
+    },
+  }
+
+  // const pie = {
+  //   labels: Object.keys(tvtSplits),
+  //   values: Object.values(tvtSplits),
+  //   type: 'pie',
+  // }
+
+  const vizData = [partitionsPlot]
+  const layout = {
+    title: 'Dataset partitions',
+    autosize: true,
+    font: {
+      size: 18,
+    },
+    // height: 1000,
+    yaxis: {
+      title: 'Count',
+      automargin: true,
+    },
+    xaxis: {
+      title: 'Partitions',
+      //   type: 'log',
+      //   automargin: true,
+      //   tick0: 0,
+      //   dtick: Math.log10(1), //log10(e**1), 0.30102999566
+    },
+  }
+
+  Plotly.newPlot('plot1', vizData, layout, {scrollZoom: true})
+  // Plotly.newPlot('plot', [pie], layout);
 }
 
 const build = async () => {
-  const [/* categories,  */ data] = await loadData()
+  const [data] = await loadData()
   console.log(data)
-  const q1Plot = {
-    x: Object.keys(data),
-    y: Object.values(data).map(ctr => ctr.overall),
-    type: 'bar',
-  }
-
-  const vizData = [q1Plot]
-  const layout = {
-    title: 'Label Distribution',
-  }
-
-  // eslint-disable-next-line no-undef
-  Plotly.newPlot('plot', vizData, layout, {scrollZoom: true})
+  const entries = Object.entries(data)
+  labelDistributionPlot(entries)
+  tvtPartitionsPlot(entries)
 }
 
 build()
---
+++ public/utils.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-console */
+const loadData = async () => {
+  try {
+    const data = await Promise.all([
+      // fetch('../src/categories.json').then(res => res.json(), console.error),
+      // fetch('../src/labels.json').then(res => res.json(), console.error),
+      fetch('../playground/categoryPartitions.json').then(
+        res => res.json(),
+        console.error,
+      ),
+    ])
+
+    return data //[categories, dataset, categoryPartitions]
+  } catch (error) {
+    console.log('Error downloading one or more files:', error)
+  }
+}
+
+//Source: https://www.30secondsofcode.org/js/s/order-by/
+const orderBy = (arr, props, orders) =>
+  [...arr].sort((a, b) =>
+    props.reduce((acc, prop, i) => {
+      if (acc === 0) {
+        const [p1, p2] =
+          orders && orders[i] === 'desc'
+            ? [b[prop], a[prop]]
+            : [a[prop], b[prop]]
+        acc = p1 > p2 ? 1 : p1 < p2 ? -1 : 0
+      }
+      return acc
+    }, 0),
+  )
+
+const orderEntriesByValues = (arr, props, orders) =>
+  [...arr].sort(([, a], [, b]) =>
+    props.reduce((acc, prop, i) => {
+      if (acc === 0) {
+        const [p1, p2] =
+          orders && orders[i] === 'desc'
+            ? [b[prop], a[prop]]
+            : [a[prop], b[prop]]
+        acc = p1 > p2 ? 1 : p1 < p2 ? -1 : 0
+      }
+      return acc
+    }, 0),
+  )
+
+const perc = num => `${Math.round(num * 10000) / 100}%`
+
+export {loadData, orderBy, orderEntriesByValues, perc}
